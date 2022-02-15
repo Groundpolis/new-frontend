c688e10f962b037008e15fddce379f1c598d40a7
Xeltica <xeltica@gmail.com>
2022-02-15 11:33:51 +0900

    [アバター] 猫耳の色にblurhashを使うように&リンク対応

--- src/components/common/Avatar.tsx
+++ src/components/common/Avatar.tsx
@@ -1,6 +1,9 @@
+import { toString } from 'misskey-js/built/acct';
 import { UserDetailed } from 'misskey-js/built/entities';
-import React from 'react';
+import React, { useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
+import { extractAvgColorFromBlurhash } from '../../scripts/extract-avg-color-from-blurhash';
 
 const earwiggleleft = keyframes`
 	from { transform: rotate(37.6deg) skew(30deg); }
@@ -17,7 +20,7 @@ const earwiggleright = keyframes`
 	to { transform: rotate(-37.6deg) skew(-30deg); }
 `;
 
-const AvatarWrapper = styled.div<{size: string, inline?: boolean}>`
+const AvatarWrapper = styled.div<{size: string, inline?: boolean, earColor: string}>`
 display: ${p => p.inline ? 'inline-block' : 'block'};
 position: relative;
 width: ${props => props.size};
@@ -26,7 +29,7 @@ background-size: cover;
 &.cat {
   &:before, &:after {
       background: #df548f;
-      border: solid 4px currentColor;
+      border: solid 4px ${p => p.earColor};
       box-sizing: border-box;
       content: '';
       display: inline-block;
@@ -56,12 +59,15 @@ export type AvatarProp = {
     user: UserDetailed,
     size?: number | string,
     inline?: boolean,
+    noLink?: boolean,
 };
 
 export default function Avatar(prop: AvatarProp) {
   const size = !prop.size ? '64px' : typeof prop.size === 'string' ? prop.size : prop.size + 'px';
-  return (
-    <AvatarWrapper size={size} inline={prop.inline} className={prop.user.isCat ? 'animated cat' : ''}>
+
+  const avgColor = useMemo(() => extractAvgColorFromBlurhash(prop.user.avatarBlurhash), [prop.user.avatarBlurhash]);
+  const inner = useMemo(() => (
+    <AvatarWrapper size={size} inline={prop.inline} className={prop.user.isCat ? 'animated cat' : ''} earColor={avgColor ?? 'var(--fg)'}>
       <img src={prop.user.avatarUrl} className="circle" style={{
         position: 'absolute',
         inset: 0,
@@ -70,5 +76,7 @@ export default function Avatar(prop: AvatarProp) {
         zIndex: 10,
       }} />
     </AvatarWrapper>
-  );
+  ), [size, prop.inline, prop.user.isCat, avgColor, prop.user.avatarUrl]);
+
+  return prop.noLink ? inner : <Link to={`/@${toString(prop.user)}`}>{inner}</Link>;
 }
