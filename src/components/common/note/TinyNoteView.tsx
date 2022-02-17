ebd3c6d26d9ea4410cad6fdbdc3ee043e238f354
Ebise Lutica <7106976+Xeltica@users.noreply.github.com>
2022-02-17 11:24:41 +0900

    引用のCWが無視される不具合を修正

--- src/components/common/note/TinyNoteView.tsx
+++ src/components/common/note/TinyNoteView.tsx
@@ -1,8 +1,8 @@
 import { Note, UserDetailed } from 'misskey-js/built/entities';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Avatar from '../Avatar';
-import { Gpfm } from '../Gpfm';
+import { GpfmView } from '../GpfmView';
 import NoteHeader from './NoteHeader';
 import { BodyWrapper } from './NoteView';
 
@@ -22,15 +22,26 @@ export type TinyNoteViewProp = {
 };
 
 export default function TinyNoteView({note, isReply, noLink}: TinyNoteViewProp) {
+  const [isCwOpened, setCwOpened] = useState(false);
+  const isVisibleBody = !note.cw || isCwOpened;
+
   return (
     <Container>
       <Avatar user={note.user as UserDetailed} size={48} />
       <main style={{flex: 1, minWidth: 0}}>
         <NoteHeader note={note} noLink={noLink} />
-        {note.text && (
+        {note.cw && (
+          <aside className="mt-1">
+            <GpfmView text={note.cw} emojis={note.emojis} />
+            <button className="btn flat text-75 ml-1 text-white" style={{padding: '4px 8px', background: 'var(--tone-4)'}} onClick={() => setCwOpened(!isCwOpened)}>
+              {isCwOpened ? '隠す' : `もっと見る(${note.text?.length ?? 0}文字${note.files && note.files.length > 0 ? ', ' + note.files.length + 'ファイル' : ''})`}
+            </button>
+          </aside>
+        )}
+        {isVisibleBody && note.text && (
           <BodyWrapper className="mt-1">
             {isReply && <i className="fas fa-reply text-primary"/>}
-            <Gpfm text={note.text}/>
+            <GpfmView text={note.text}/>
           </BodyWrapper>
         )}
       </main>
